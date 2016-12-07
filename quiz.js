6dc110b2b6c120b63da6dc3ce4b41fb851ed08a7
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2016-12-07 08:36:42 -1000

    kms

---
+++ quiz.js
@@ -0,0 +1,17 @@
+var myArray = ["string1", "string2", 10, true];
+
+myArray[2]
+
+console.log(10)
+function gang(parameter) {
+  if (parameter = 10) {
+    console.log("i has no time")
+  else 
+    console.log("you shall not pass")
+  }
+}
+
+myArray(parameter)
+
+var batman = {man:true, utilityBelt:["weapon1","hi"], vehicle:batmobile, sidekick:robin};
+
